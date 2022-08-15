d1ad811afce021336e75dcb2c8527c7ea5baa171
Handy Setiawan <handy7119@gmail.com>
2022-08-15 16:35:36 +0700

    Mengganti jarak warna header muncul

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const fungsiScroll = function scrollFunction() {
-  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
     document.getElementById('header').style.backgroundColor = "#282A35";
   } else {
     document.getElementById('header').style.backgroundColor = "transparent";
